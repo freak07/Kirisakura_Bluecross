7bbb03606cb34903e554fee642c1a64e62eeeb6b
jiad <jiad@codeaurora.org>
2018-04-19 17:47:34 +0800

    qcacld-3.0: Add set_perf_profile IPA WDI unified API
    IPA driver adds unified WDI API for ipa_wdi_set_perf_profile.
    WLAN driver needs to adopt the change accordingly.
    
    Change-Id: Ieaaf7cf5231f74cd6ba358c02032ae173d9618f0
    CRs-Fixed: 2211504

--- core/hdd/src/wlan_hdd_ipa.c
+++ core/hdd/src/wlan_hdd_ipa.c
@@ -1404,11 +1404,15 @@ static inline int hdd_ipa_wdi_teardown_sys_pipe(struct hdd_ipa_priv *hdd_ipa,
 	return 0;
 }
 
-static inline int hdd_ipa_wdi_rm_set_perf_profile(struct hdd_ipa_priv *hdd_ipa,
-		enum ipa_rm_resource_name resource_name,
-		struct ipa_rm_perf_profile *profile)
+static int hdd_ipa_wdi_rm_set_perf_profile(struct hdd_ipa_priv *hdd_ipa,
+		int client, uint32_t max_supported_bw_mbps)
 {
-	return 0;
+	struct ipa_wdi_perf_profile profile;
+
+	profile.client = client;
+	profile.max_supported_bw_mbps = max_supported_bw_mbps;
+
+	return ipa_wdi_set_perf_profile(&profile);
 }
 
 static inline int hdd_ipa_wdi_rm_request_resource(struct hdd_ipa_priv *hdd_ipa,
@@ -2262,10 +2266,24 @@ static int hdd_ipa_wdi_teardown_sys_pipe(struct hdd_ipa_priv *hdd_ipa,
 }
 
 static int hdd_ipa_wdi_rm_set_perf_profile(struct hdd_ipa_priv *hdd_ipa,
-		enum ipa_rm_resource_name resource_name,
-		struct ipa_rm_perf_profile *profile)
+		int client, uint32_t max_supported_bw_mbps)
 {
-	return ipa_rm_set_perf_profile(resource_name, profile);
+	enum ipa_rm_resource_name resource_name;
+	struct ipa_rm_perf_profile profile;
+
+	if (client == IPA_CLIENT_WLAN1_PROD) {
+		resource_name = IPA_RM_RESOURCE_WLAN_PROD;
+	} else if (client == IPA_CLIENT_WLAN1_CONS) {
+		resource_name = IPA_RM_RESOURCE_WLAN_CONS;
+	} else {
+		HDD_IPA_LOG(QDF_TRACE_LEVEL_ERROR,
+				"not supported client: %d", client);
+		return -EINVAL;
+	}
+
+	profile.max_supported_bandwidth_mbps = max_supported_bw_mbps;
+
+	return ipa_rm_set_perf_profile(resource_name, &profile);
 }
 
 static int hdd_ipa_wdi_rm_request_resource(struct hdd_ipa_priv *hdd_ipa,
@@ -5288,7 +5306,6 @@ static int __hdd_ipa_set_perf_level(hdd_context_t *hdd_ctx, uint64_t tx_packets,
 {
 	uint32_t next_cons_bw, next_prod_bw;
 	struct hdd_ipa_priv *hdd_ipa;
-	struct ipa_rm_perf_profile profile;
 	int ret;
 
 	if (wlan_hdd_validate_context(hdd_ctx))
@@ -5299,8 +5316,6 @@ static int __hdd_ipa_set_perf_level(hdd_context_t *hdd_ctx, uint64_t tx_packets,
 	if ((!hdd_ipa_is_enabled(hdd_ctx)) ||
 		(!hdd_ipa_is_clk_scaling_enabled(hdd_ctx)))
 		return 0;
-
-	memset(&profile, 0, sizeof(profile));
 
 	if (tx_packets > (hdd_ctx->config->busBandwidthHighThreshold / 2))
 		next_cons_bw = hdd_ctx->config->IpaHighBandwidthMbps;
@@ -5328,9 +5343,8 @@ static int __hdd_ipa_set_perf_level(hdd_context_t *hdd_ctx, uint64_t tx_packets,
 	if (hdd_ipa->curr_cons_bw != next_cons_bw) {
 		hdd_debug("Requesting CONS perf curr: %d, next: %d",
 			    hdd_ipa->curr_cons_bw, next_cons_bw);
-		profile.max_supported_bandwidth_mbps = next_cons_bw;
 		ret = hdd_ipa_wdi_rm_set_perf_profile(hdd_ipa,
-				IPA_RM_RESOURCE_WLAN_CONS, &profile);
+				IPA_CLIENT_WLAN1_CONS, next_cons_bw);
 		if (ret) {
 			hdd_err("RM CONS set perf profile failed: %d", ret);
 
@@ -5343,9 +5357,8 @@ static int __hdd_ipa_set_perf_level(hdd_context_t *hdd_ctx, uint64_t tx_packets,
 	if (hdd_ipa->curr_prod_bw != next_prod_bw) {
 		hdd_debug("Requesting PROD perf curr: %d, next: %d",
 			    hdd_ipa->curr_prod_bw, next_prod_bw);
-		profile.max_supported_bandwidth_mbps = next_prod_bw;
 		ret = hdd_ipa_wdi_rm_set_perf_profile(hdd_ipa,
-				IPA_RM_RESOURCE_WLAN_PROD, &profile);
+				IPA_CLIENT_WLAN1_PROD, next_prod_bw);
 		if (ret) {
 			hdd_err("RM PROD set perf profile failed: %d", ret);
 			return ret;
@@ -7195,7 +7208,6 @@ static QDF_STATUS __hdd_ipa_init(hdd_context_t *hdd_ctx)
 	int ret, i;
 	struct hdd_ipa_iface_context *iface_context = NULL;
 	struct ol_txrx_pdev_t *pdev = NULL;
-	struct ipa_rm_perf_profile profile;
 
 	HDD_IPA_LOG(QDF_TRACE_LEVEL_DEBUG, "enter");
 
@@ -7287,19 +7299,18 @@ static QDF_STATUS __hdd_ipa_init(hdd_context_t *hdd_ctx)
 
 	/* When IPA clock scaling is disabled, initialze maximum clock */
 	if (!hdd_ipa_is_clk_scaling_enabled(hdd_ctx)) {
-		profile.max_supported_bandwidth_mbps = HDD_IPA_MAX_BANDWIDTH;
 		hdd_debug("IPA clock scaling is disabled.");
 		hdd_debug("Set initial CONS/PROD perf: %d",
-			  profile.max_supported_bandwidth_mbps);
+				HDD_IPA_MAX_BANDWIDTH);
 		ret = hdd_ipa_wdi_rm_set_perf_profile(hdd_ipa,
-				IPA_RM_RESOURCE_WLAN_CONS, &profile);
+				IPA_CLIENT_WLAN1_CONS, HDD_IPA_MAX_BANDWIDTH);
 		if (ret) {
 			hdd_err("RM CONS set perf profile failed: %d", ret);
 			goto fail_create_sys_pipe;
 		}
 
 		ret = hdd_ipa_wdi_rm_set_perf_profile(hdd_ipa,
-				IPA_RM_RESOURCE_WLAN_PROD, &profile);
+				IPA_CLIENT_WLAN1_PROD, HDD_IPA_MAX_BANDWIDTH);
 		if (ret) {
 			hdd_err("RM PROD set perf profile failed: %d", ret);
 			goto fail_create_sys_pipe;

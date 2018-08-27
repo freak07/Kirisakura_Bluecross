88b4aab37ee729b9c106626cead4d82fa59bd5d2
Karthik Anantha Ram <kartanan@codeaurora.org>
2018-08-27 10:09:55 -0700

    msm: camera: reqmgr: Remove mem_mgr init & deinit calls
    Remove the calls to init & deinit the mem_mgr as part of
    driver probe and remove. The init & deinit is already
    being done as part of open & release v4l2 file ops.
    
    Change-Id: Ic97cb8b628f3406d88b21d914cd7b4f64b9d833c
    Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>
    (cherry picked from commit 34b6b40f16a1ffa692f62265934cda972773cd21)
    (cherry picked from commit 3ae592cb7e6b3e8b7d30b64a81e69c3c21d214b4)

--- drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_dev.c
+++ drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_dev.c
@@ -586,7 +586,6 @@ static int cam_req_mgr_remove(struct platform_device *pdev)
 	kmem_cache_destroy(g_cam_req_mgr_timer_cachep);
 	g_cam_req_mgr_timer_cachep = NULL;
 	cam_req_mgr_core_device_deinit();
-	cam_mem_mgr_deinit();
 	cam_req_mgr_util_deinit();
 	cam_media_device_cleanup();
 	cam_video_device_cleanup();
@@ -625,12 +624,6 @@ static int cam_req_mgr_probe(struct platform_device *pdev)
 		goto req_mgr_util_fail;
 	}
 
-	rc = cam_mem_mgr_init();
-	if (rc) {
-		CAM_ERR(CAM_CRM, "mem mgr init failed");
-		goto mem_mgr_init_fail;
-	}
-
 	rc = cam_req_mgr_core_device_init();
 	if (rc) {
 		CAM_ERR(CAM_CRM, "core device setup failed");
@@ -654,8 +647,6 @@ static int cam_req_mgr_probe(struct platform_device *pdev)
 	return rc;
 
 req_mgr_core_fail:
-	cam_mem_mgr_deinit();
-mem_mgr_init_fail:
 	cam_req_mgr_util_deinit();
 req_mgr_util_fail:
 	mutex_destroy(&g_dev.dev_lock);

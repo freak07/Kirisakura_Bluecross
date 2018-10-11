f7f1cc17425890e8cf434f4b02c673bfec1439e9
Harsh Shah <harshs@codeaurora.org>
2018-10-11 12:15:12 -0700

    msm: camera: isp: Delay the change in BW during stop
    Change the sequence of reducing the BW and stopping resources
    during stop_hw to ensure BW is high until all the resources are
    stopped. This will ensure that there are no overflow errors related
    to BW during stop.
    
    Change-Id: I0a4a74d56dc826ea3c0ad8f73d67ee2a16f22566
    Signed-off-by: Harsh Shah <harshs@codeaurora.org>
    (cherry picked from commit f84189bb7993cd901e0aeb53630b3ac89fcc666a)
    (cherry picked from commit 87114f3b3b8da6bae26a52d8bc675516c15b8d9b)

--- drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
+++ drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -1961,13 +1961,6 @@ static int cam_ife_mgr_stop_hw(void *hw_mgr_priv, void *stop_hw_args)
 		CAM_ERR(CAM_ISP, "CDM stream off failed %d",
 			ctx->cdm_handle);
 
-	CAM_DBG(CAM_ISP, "Going to stop IFE Mux");
-
-	/* IFE mux in resources */
-	list_for_each_entry(hw_mgr_res, &ctx->res_list_ife_src, list) {
-		cam_ife_hw_mgr_stop_hw_res(hw_mgr_res);
-	}
-
 	CAM_DBG(CAM_ISP, "Going to stop IFE Out");
 
 	/* IFE out resources */
@@ -1980,6 +1973,13 @@ static int cam_ife_mgr_stop_hw(void *hw_mgr_priv, void *stop_hw_args)
 			master_base_idx = ctx->base[i].idx;
 			break;
 		}
+	}
+
+	CAM_DBG(CAM_ISP, "Going to stop IFE Mux");
+
+	/* IFE mux in resources */
+	list_for_each_entry(hw_mgr_res, &ctx->res_list_ife_src, list) {
+		cam_ife_hw_mgr_stop_hw_res(hw_mgr_res);
 	}
 
 	/* After this, there will be no BH call so that we can call
@@ -2015,7 +2015,7 @@ static int cam_ife_mgr_stop_hw(void *hw_mgr_priv, void *stop_hw_args)
 	cam_ife_mgr_csid_stop_hw(ctx, &ctx->res_list_ife_cid,
 			master_base_idx, csid_halt_type);
 
-	/* stop rest of the CIDs  */
+	/* stop rest of the CIDs */
 	for (i = 0; i < ctx->num_base; i++) {
 		if (ctx->base[i].idx == master_base_idx)
 			continue;
@@ -2024,6 +2024,8 @@ static int cam_ife_mgr_stop_hw(void *hw_mgr_priv, void *stop_hw_args)
 		cam_ife_mgr_csid_stop_hw(ctx, &ctx->res_list_ife_cid,
 			ctx->base[i].idx, csid_halt_type);
 	}
+
+	cam_ife_mgr_pause_hw(ctx);
 
 	if (stop_isp->stop_only)
 		goto end;

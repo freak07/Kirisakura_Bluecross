7a22288b1ac6095a2275f71af019d30a39b498c1
freak07 <michalechner92@googlemail.com>
2019-04-26 12:35:48 +0200

    Revert "sched: rt: Use RCU lock in rt task cpu select path"
    This reverts commit 0cdc29e21c95dac9eaeacc47a265f6fb1e7e34d3.

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1803,6 +1803,7 @@ static int find_lowest_rq(struct task_struct *task)
 				}
 			}
 		} while (sg = sg->next, sg != sd->groups);
+		rcu_read_unlock();
 
 		if (sg_target) {
 			cpumask_and(&search_cpu, lowest_mask,
@@ -1891,7 +1892,6 @@ retry:
 		}
 
 		if (best_cpu != -1 && placement_boost != SCHED_BOOST_ON_ALL) {
-			rcu_read_unlock();
 			return best_cpu;
 		} else if (!cpumask_empty(&backup_search_cpu)) {
 			cpumask_copy(&search_cpu, &backup_search_cpu);
@@ -1900,7 +1900,6 @@ retry:
 			placement_boost = SCHED_BOOST_NONE;
 			goto retry;
 		}
-		rcu_read_unlock();
 	}
 
 noea:

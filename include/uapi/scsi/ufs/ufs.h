MotorolaMobilityLLC/kernel-msm

scsi: ufs: update dev ref clock gating wait time
This change updates the wait time before turning off the reference clock.
The wait time of UFS version 3.0 and onward devices can be read out from
their device attributes. Otherwise, fix the wait time to 100 micro seconds.

Change-Id: Ie5085e62ef3efa2bfaa3f632593e4ade52ffd645
Signed-off-by: Can Guo <cang@codeaurora.org>

include/uapi/scsi/ufs/ufs.h
@@ -36,7 +36,11 @@ enum attr_idn {
 	QUERY_ATTR_IDN_SECONDS_PASSED		= 0x0F,
 	QUERY_ATTR_IDN_CNTX_CONF		= 0x10,
 	QUERY_ATTR_IDN_CORR_PRG_BLK_NUM		= 0x11,
+	QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME	= 0x17,
 };
+
+#define QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME \
+				QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME
 
 #define QUERY_ATTR_IDN_BOOT_LU_EN_MAX	0x02
 

freak07/Kirisakura_Bluecross

Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>
(cherry picked from commit 4e774f07d587adaa0eaaf8186d7135721e22800e)
(cherry picked from commit d3393e3f8b28a17c8710a38e0bc6af36f3f3f41e)
(cherry picked from commit b800eb61f9da065812092b539dfe7dece34e7c4d)
(cherry picked from commit 6fb77d4765dc3b0b594f1552320b6e636a4a35a6)

include/uapi/scsi/ufs/ufs.h
@@ -40,7 +40,11 @@ enum attr_idn {
 	QUERY_ATTR_IDN_CORR_PRG_BLK_NUM		= 0x11,
 	/* use one reserved bit */
 	QUERY_ATTR_IDN_MANUAL_GC_STATUS		= 0x17,
+	QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME	= 0x18,
 };
+
+#define QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME \
+				QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME
 
 #define QUERY_ATTR_IDN_BOOT_LU_EN_MAX	0x02
 
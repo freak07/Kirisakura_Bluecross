090788db1873af795b5e371f969e659aef9fb330
freak07 <michalechner92@googlemail.com>
2019-07-13 18:08:16 +0200

    buildscript: add simple buildscript
    (cherry picked from commit e5b462984bdaf82f42560449122db01d3f2509ff)

---
+++ build_kirisakura.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo
+echo "Clean Build Directory"
+echo 
+
+make clean && make mrproper
+
+echo
+echo "Issue Build Commands"
+echo
+
+mkdir -p out
+export ARCH=arm64
+export SUBARCH=arm64
+export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r353983e/bin
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+
+echo
+echo "Set DEFCONFIG"
+echo 
+# make CC=clang O=out kirisakura_defconfig
+make CC=clang O=out b1c1_defconfig
+
+echo
+echo "Build The Good Stuff"
+echo 
+
+make CC=clang O=out -j16

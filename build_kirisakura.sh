b322775ee6489ed787e54adef61c2434392985bf
freak07 <michalechner92@googlemail.com>
2019-09-19 08:26:49 +0200

    buildscript: add ld_library_path
    this change is needed as I no longer keep the lib in /usr/lib64

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -18,6 +18,7 @@ export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LD_LIBRARY_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r353983e/lib64:$LD_LIBRARY_PATH
 
 echo
 echo "Set DEFCONFIG"

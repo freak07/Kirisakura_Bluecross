a6f8378ac3778b35a3d7add1af88ccc4354bbc60
gaurank kathpalia <gkathpal@codeaurora.org>
2019-11-06 12:43:35 +0530

    qcacld-3.0: Use variable size structure to store channel in scan req
    Currently the array of scan channel list is of size
    SIR_ESE_MAX_MEAS_IE_REQS, but the memory is allocated dynamically
    for the channge which can be greater than SIR_ESE_MAX_MEAS_IE_REQS.
    
    So use dynamic array for this as memory is allocated for this every
    time.
    
    bug: 139058079
    Change-Id: I3c854b339c49d9f628033aa6742d57568ec14954
    CRs-Fixed: 2560184
    Signed-off-by: Isaac Chiou <isaacchiou@google.com>

--- core/mac/src/pe/lim/lim_ft_preauth.c
+++ core/mac/src/pe/lim/lim_ft_preauth.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2019 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -672,7 +672,8 @@ QDF_STATUS lim_send_preauth_scan_offload(tpAniSirGlobal mac_ctx,
 	tSirRetStatus rc = eSIR_SUCCESS;
 	tSirMsgQ msg;
 
-	scan_offload_req = qdf_mem_malloc(sizeof(tSirScanOffloadReq));
+	scan_offload_req = qdf_mem_malloc(sizeof(tSirScanOffloadReq) +
+					  sizeof(uint8_t));
 	if (NULL == scan_offload_req) {
 		pe_err("Memory allocation failed for pScanOffloadReq");
 		return QDF_STATUS_E_NOMEM;

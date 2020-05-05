633fdc15fa5545ec94198c6f502b0b273ecaa311
freak07 <michalechner92@googlemail.com>
2020-05-05 12:03:28 +0200

    media: camera: fix clang 11 error

--- drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
+++ drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -1114,15 +1114,16 @@ static int cam_ife_hw_mgr_acquire_res_ife_csid_rdi(
 
 	for (i = 0; i < in_port->num_out_res; i++) {
 		out_port = &in_port->data[i];
-		if (!cam_ife_hw_mgr_is_rdi_res(out_port->res_type))
+		
+	if (!cam_ife_hw_mgr_is_rdi_res(out_port->res_type)) {
 			continue;
-
+	}
 			/* get cid resource */
 			rc = cam_ife_mgr_acquire_cid_res(ife_ctx,
 				in_port, &cid_res_id,
 				cam_ife_hw_mgr_get_ife_csid_rdi_res_type(
 				out_port->res_type));
-			if (rc) {
+		if (rc) {
 				CAM_ERR(CAM_ISP,
 					"Acquire IFE CID resource Failed");
 				goto err;

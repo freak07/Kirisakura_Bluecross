6e6a9bdf7b859d989a1e0d8329b9900a11bfc968
freak07 <michalechner92@googlemail.com>
2020-07-03 12:45:26 +0200

    buildscript: switch to clang 11

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -13,16 +13,16 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r353983e/bin
+export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r383902c/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LD_LIBRARY_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r383902c/lib64:$LD_LIBRARY_PATH
 
 echo
 echo "Set DEFCONFIG"
 echo 
-# make CC=clang O=out kirisakura_defconfig
 make CC=clang O=out b1c1_defconfig
 
 echo
